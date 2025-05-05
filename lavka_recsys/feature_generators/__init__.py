c29e33e1b24ae4cfc7958660aa90016137bd60fd
Alexander Mayorov <mayorovme@gmail.com>
2025-05-05 23:41:44 +0000

    moved targets to a separate file

--- lavka_recsys/feature_generators/__init__.py
+++ lavka_recsys/feature_generators/__init__.py
@@ -2,6 +2,7 @@ from .collaborative_filtering import register_cf_fgens
 from .common import register_common_fgens
 from .text_processor import register_text_embedding_fgens
 from .bpr import register_bpr_fgens
+from .targets import register_target_fgens
 
 
 def register_all_fgens():
@@ -12,3 +13,4 @@ def register_all_fgens():
     register_text_embedding_fgens()
     register_common_fgens()
     register_bpr_fgens()
+    register_target_fgens()
---
+++ lavka_recsys/feature_generators/targets.py
@@ -0,0 +1,54 @@
+from ..feature_factory import FeatureFactory
+
+import polars as pl
+
+def register_target_fgens():
+    @FeatureFactory.register_target('CartUpdate_vs_View')
+    def target(history_df: pl.DataFrame, target_df: pl.DataFrame) -> pl.Series:
+        """Assign 0 for 'AT_View' and 1 for 'AT_CartUpdate'."""
+        mapping = {
+            'AT_View': 0,
+            'AT_CartUpdate': 1,
+        }
+        return target_df.with_columns(
+            target=pl.col("action_type").map_elements(
+                lambda x: mapping.get(x, None),
+                return_dtype=pl.Int64
+            )
+        )['target']
+
+    @FeatureFactory.register_target('CartUpdate_Purchase_vs_View')
+    def target_cart_update_purchase(history_df: pl.DataFrame, target_df: pl.DataFrame) -> pl.Series:
+        """Assign 0 for 'AT_View' and 1 for 'AT_CartUpdate' and 'AT_Purchase'."""
+        mapping = {
+            'AT_View': 0,
+            'AT_CartUpdate': 1,
+            'AT_Purchase': 1,
+        }
+        return target_df.with_columns(
+            target=pl.col("action_type").map_elements(
+                lambda x: mapping.get(x, None),
+                return_dtype=pl.Int64
+            )
+        )['target']
+
+    @FeatureFactory.register_target('CartUpdate_conversion_aware')
+    def target(history_df: pl.DataFrame, target_df: pl.DataFrame) -> pl.Series:
+        """Assign 0 for 'AT_View' and 1 for 'AT_CartUpdate' and 'AT_Purchase'."""
+        conversion_action_types = ["AT_CartUpdate", "AT_Purchase", "AT_Click"]
+        target_df = target_df.with_columns(
+            conv=pl.col("action_type")
+                .is_in(conversion_action_types)
+                .any()  # any returns True if at least one conversion event is present
+                .over(["request_id", "product_id"])
+        )
+        target_series = target_df.with_columns(
+            target=pl.when(pl.col("action_type").is_in(conversion_action_types))
+                        .then(1)
+                        .otherwise(
+                            pl.when((pl.col("action_type") == "AT_View") & (~pl.col("conv")))
+                            .then(0)
+                            .otherwise(None)
+                        )
+        )["target"]
+        return target_series
